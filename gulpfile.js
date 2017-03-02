5f77b09ff2a6dabcb1c1d565a1e4bb11e2d5fb26
jaroslaw.rewers <jaroslaw.rewers@cognifide.com>
2017-03-02 15:28:55 +0100

    finished requestes

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,24 @@ gulp.task('sass-concat', () => {
         .pipe(gulp.dest('./build/css'))
 })
 
+gulp.task('sass', () => {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'))
+})
+
 gulp.task('inlineimg', () => {
     return gulp.src('./index-jpg-responsive.html')
         .pipe(inlineimg('./'))
         .pipe(rename({suffix: '-base64'}))
         .pipe(gulp.dest('./'))
+})
+
+gulp.task('js', () => {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']}))
+        .pipe(gulp.dest('./build/js'))
 })
 
 gulp.task('js-concat', () => {
