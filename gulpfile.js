ab6bb427a96af9e73351c0858eb27718215ccfaf
jaroslaw.rewers <jaroslaw.rewers@cognifide.com>
2017-03-02 15:00:35 +0100

    refactored images

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     inlineimg = require('gulp-inline-image-html'),
-    rename = require('gulp-rename')
+    rename = require('gulp-rename'),
+    babel = require('gulp-babel')
 
 gulp.task('sass-concat', () => {
     return gulp.src('./src/sass/**/*.scss')
+        .pipe(concat('styles.css'))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./build/css'))
 })
@@ -15,4 +17,12 @@ gulp.task('inlineimg', () => {
         .pipe(inlineimg('./'))
         .pipe(rename({suffix: '-base64'}))
         .pipe(gulp.dest('./'))
+})
+
+gulp.task('js-concat', () => {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(babel({
+            presets: ['es2015']}))
+        .pipe(gulp.dest('./build/js'))
 })
