96bff53e1399fd7b2bdb3b667ae3ee5041e23ceb
Jarosław Rewers <jaroslaw.rewers@gmail.com>
2017-03-11 16:16:04 +0100

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,24 @@ gulp.task('sass-concat', () => {
         .pipe(gulp.dest('./build/css'))
 })
 
+gulp.task('sass', () => {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'))
+})
+
 gulp.task('inlineimg', () => {
     return gulp.src('./index-jpg-responsive.html')
         .pipe(inlineimg('./'))
         .pipe(rename({suffix: '-base64'}))
         .pipe(gulp.dest('./'))
+})
+
+gulp.task('js', () => {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']}))
+        .pipe(gulp.dest('./build/js'))
 })
 
 gulp.task('js-concat', () => {
@@ -25,4 +38,14 @@ gulp.task('js-concat', () => {
         .pipe(babel({
             presets: ['es2015']}))
         .pipe(gulp.dest('./build/js'))
+})
+
+gulp.task('watch-concat', () => {
+    gulp.watch('./src/js/**/*', ['js-concat'])
+    gulp.watch('./src/sass/**/*.scss', ['sass-concat'])
+})
+
+gulp.task('watch', () => {
+    gulp.watch('./src/js/**/*', ['js'])
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
 })
